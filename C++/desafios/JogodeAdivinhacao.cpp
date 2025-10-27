6c515a200c53b208a8da80266c633fd1e27c1204
Vinicim <vinicius_anacleto@estudante.sesisenai.org.br>
2025-10-27 19:32:20 -0300

    Jogo de adivinhação
    Mais um desafio feito, onde o código ira gerar um numero aleatório entre 1 a 100, onde o usuário tenta adivinhar qual é, contendo um contador de tentativas

---
+++ C++/desafios/JogodeAdivinhacao.cpp
@@ -0,0 +1,31 @@
+//jogo de adivinhação em cpp
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    std::srand(std::time(0)); // inicializa o gerador de numeros aleatorios com a hora atual
+    int numeroSecreto = std::rand() % 100 + 1; // gera um numero aleatorio entre 1 e 100
+    int palpite;
+    int tentativas = 0;
+
+    std::cout << "Bem-vindo ao jogo de adivinhacao!" << std::endl;
+    std::cout << "Tente adivinhar o numero secreto entre 1 e 100." << std::endl;
+
+    do {
+        std::cout << "Digite seu palpite: ";
+        std::cin >> palpite;
+        tentativas++;
+
+        if (palpite < numeroSecreto) {
+            std::cout << "Muito baixo! Tente novamente." << std::endl;
+        } else if (palpite > numeroSecreto) {
+            std::cout << "Muito alto! Tente novamente." << std::endl;
+        } else {
+            std::cout << "Parabens! Voce adivinhou o numero secreto em " << tentativas << " tentativas." << std::endl;
+        }
+    } while (palpite != numeroSecreto);
+
+    return 0;
+}
