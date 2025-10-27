d25a918c1b794e3b02102dbaee2fcc96c704194e
Vinicim <vinicius_anacleto@estudante.sesisenai.org.br>
2025-10-27 18:31:04 -0300

    Finalização da calculadora
    Colocando o resto das operações na calculadora + uma mensagem de erro caso algum dado seja invalido.

--- C++/desafios/CalculadoraSimples.cpp
+++ C++/desafios/CalculadoraSimples.cpp
@@ -5,13 +5,13 @@ int main() {
     double num1, num2;
     char operacao;
 
-    std::cout << "Digite o primeiro numero a ser calculado: ";
+    std::cout << "Digite o primeiro numero a ser calculado: " << std::endl;
     std::cin >> num1;
 
-    std::cout << "Digite a operacao a ser realizada (+, -, /, *): ";
+    std::cout << "Digite a operacao a ser realizada (+, -, /, *): " << std::endl;
     std::cin >> operacao;
 
-    std::cout << "Digite o segundo numero a ser calculado: ";
+    std::cout << "Digite o segundo numero a ser calculado: " << std::endl;
     std::cin >> num2;
 
     double resultado;
@@ -21,7 +21,15 @@ int main() {
         std::cout << "A soma dos numeros é : " << resultado << std::endl;
     } else if (operacao == "-") {
         resultado = num1 - num2;
-        
+        std::cout << "A subtração dos numeros é: " << resultado << std::endl;
+    } else if (operacao == "/") {
+        resultado = num1 / num2;
+        std::cout << "A divisão dos numeros é: " << resultado << std::endl;
+    } else if (operacao == "*") {
+        resultado = num1 * num2;
+        std::cout << "A multiplicação dos numeros é: " << resultado << std::endl;
+    } else {
+        std::cout << "Dados invalidos inseridos" << std::endl;
     }
 
     return 0;
